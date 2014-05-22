ba551428c068e5754d976e7011e026aea7d74915
JanisLu <janis.ludborzs@ms-idi.eu>
2014-05-22 12:21:26 +0300

    Tree example

---
+++ js/jstree-methods.js
@@ -0,0 +1,87 @@
+﻿////////////////////////////////////////////////////
+/*
+ * JSTree methods
+ */
+
+(function ($) {
+
+    $.treeViewDataKey = 'treeviewData6';
+    $.prepareStartupData = function () {
+
+        if (window.localStorage && localStorage.getItem($.treeViewDataKey) == null) {
+            var startupData = [
+                        {
+                            "data": {
+                                "title": "Node1"
+                            },
+                            "attr": { "id": "node1" },
+                            "children": [
+                                {
+                                    "data": {
+                                        "title": "Level 1"
+                                    },
+                                    "attr": { "rel": "product-category" }
+                                }
+                            ]
+                        },
+                        {
+                            "data": {
+                                "title": "Node 2"
+                            }
+                        },
+                        {
+                            "data": {
+                                "title": "Node 3"
+                            }
+                        },
+                        {
+                            "data": {
+                                "title": "Node 4"
+                            }
+                        }
+            ];
+
+            localStorage.setItem($.treeViewDataKey, JSON.stringify(startupData));
+        }
+
+    };
+
+    $.getTreeDataFromStorage = function (key) {
+
+        /* check if browser support local storage */
+        if (window.localStorage) {
+
+            var treeData = localStorage.getItem(key);
+            if (treeData == null) {
+                treeData = localStorage.getItem($.treeViewDataKey);
+            }
+            return treeData && JSON.parse(treeData);
+        }
+        else {
+            console.log("HTML5 not supported");
+            /* use some other methods to get data */
+        }
+    };
+
+    $.saveTreeData = function (key, data) {
+        
+        if (window.localStorage) {
+            localStorage.setItem(key, JSON.stringify(data));
+        }
+        else {
+            console.log("HTML5 not supported");
+            /* use some other methods to get data */
+        }
+    },
+
+    $.reloadTreeData = function (key) {
+        if (window.localStorage) {
+            return localStorage.getItem(key);
+        }
+        else {
+            console.log("HTML5 not supported");
+            /* use some other methods to get data */
+        }
+    }
+
+}(jQuery));
